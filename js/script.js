e3819a7148bcb8996683dfd02edfc81628f06c2f
Marcin Dybaś <marcin.dybas@htdevelopers.com>
2021-05-03 00:57:58 +0200

    fix: don't wait for previous animation to end

--- js/script.js
+++ js/script.js
@@ -13,10 +13,10 @@ function changeMenu(currentFrame, lastFrame, animStart, animEnd, repeatAnimStart
 
   // repeat animation
   if (lastFrame === animEnd) {
-    navBar.playSegments([repeatAnimStart, repeatAnimEnd], false);
+    navBar.playSegments([repeatAnimStart, repeatAnimEnd], true);
   // exit + entry animation
   } else if (currentFrame !== 0) {
-    navBar.playSegments([currentFrame, currentFrame + exitAnimDuration], false);
+    navBar.playSegments([currentFrame, currentFrame + exitAnimDuration], true);
     navBar.playSegments([animStart, animEnd], false);
   // entry animation
   } else {
